6bc51f2b73fba33cc30b7280d6cd76ae5788e4bf
xianghaifeng <850442777@qq.com>
2021-06-30 10:10:23 +0800

    feat: 前端自动化构建部署文档

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -25,11 +25,11 @@ if (isLocalDevelopment() === false) {
 }
 
 // Inspired by APlayer
-console.log(
-  `${'\n'} %c CALPA %c https://calpa.me ${'\n'}${'\n'}`,
-  'color: #6cf; background: #030307; padding:5px 0;',
-  'background: #6cf; padding:5px 0;',
-);
+// console.log(
+//   `${'\n'} %c CALPA %c https://calpa.me ${'\n'}${'\n'}`,
+//   'color: #6cf; background: #030307; padding:5px 0;',
+//   'background: #6cf; padding:5px 0;',
+// );
 
 export const onRouteUpdate = (state) => {
   if (isLocalDevelopment() !== true) {
